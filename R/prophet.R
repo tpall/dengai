78728404fe05eb8329eb6b1e0b30138c2afdc97b
Taavi Päll <tapa741@gmail.com>
2018-01-22 09:37:29 +0200

    imputing

--- R/prophet.R
+++ R/prophet.R
@@ -1,63 +1,67 @@
 
 library(tidyverse)
+library(modelr)
 library(prophet)
 
-train_features <- read_csv("data/dengue_features_train.csv")
-train_labs <- read_csv("data/dengue_labels_train.csv")
 
-train <- left_join(train_labs, train_features)
+dengue_features <- read_csv("data/dengue_features_train.csv")
+dengue_labs <- read_csv("data/dengue_labels_train.csv")
+dengue <- left_join(dengue_labs, dengue_features)
+dengue$week_start_date %>% is.na() %>% sum
+dengue$total_cases %>% is.na() %>% sum
 
-# Example -----------------------------------------------------------------
+library(mice)
+md.pattern(dengue)
+dengue_imp <- mice(select(dengue, -c(1:5)), method = "norm.predict", m = 1, maxit = 1)
+dengue_imp <- bind_cols(select(dengue, c(1:5)), complete(dengue_imp))
 
-# df <- read.csv('https://raw.githubusercontent.com/facebook/prophet/master/examples/example_wp_peyton_manning.csv')
-# 
-# df %>% 
-#   ggplot(aes(y)) +
-#   geom_histogram(bins = 60) +
-#   scale_fill_viridis(discrete = TRUE)
-# 
-# any(df$y == 0)
-# 
-# df <- mutate(df, y = log(y))
-# head(df)
-# 
-# m <- prophet(df)
-# future <- make_future_dataframe(m, periods = 365)
-# tail(future)
-# forecast <- predict(m, future)
-# tail(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
-# plot(m, forecast)
-# prophet_plot_components(m, forecast)
-
+dengue_nested <- dengue_imp %>%
+  select(-year, -weekofyear) %>% 
+  rename(ds = week_start_date, y = total_cases) %>% 
+  mutate(y = log(y + 1),
+    cap = case_when(
+    city == "sj" ~ log(500),
+    city == "iq" ~ log(200)
+  )) %>%
+  group_by(city) %>% 
+  nest() %>% 
+  mutate(train = map(data, ~ .x[1:(nrow(.x) - 101),]),
+         test = map(data, ~ .x[(nrow(.x) - 100):nrow(.x),]))
 
 # Dengai ------------------------------------------------------------------
 
-train_nested <- train %>% 
-  select(-year, -weekofyear) %>% 
-  rename(ds = week_start_date, y = total_cases) %>% 
-  mutate(cap = case_when(
-    city == "sj" ~ 500,
-    city == "iq" ~ 200
-  )) %>% 
-  group_by(city) %>% 
-  nest()
+dengue_nested <- dengue_nested %>% 
+  mutate(mod = map(train, 
+                   prophet, 
+                   weekly.seasonality = FALSE,
+                   daily.seasonality = FALSE,
+                   growth = 'linear',
+                   mcmc.samples = 4,
+                   fit = FALSE), 
+         mod_prec_amt = map(mod, add_regressor, name = "precipitation_amt_mm"),
+         mod_air_temp_k = map(mod, add_regressor, name = "reanalysis_air_temp_k"))
 
-train_nested$data
+dengue_nested_fits <- dengue_nested %>% 
+  mutate(mod = map2(mod, train, fit.prophet),
+         mod_prec_amt = map2(mod_prec_amt, train, fit.prophet),
+         mod_air_temp_k = map2(mod_air_temp_k, train, fit.prophet))
 
-train_nested <- train_nested %>% 
-  mutate(m = map(data, prophet, weekly.seasonality = TRUE, growth = 'logistic'))
+dengue_nested_fits <- dengue_nested_fits %>% 
+  mutate(mod_pred = map2(mod, test, predict))
 
-train_nested <- train_nested %>% 
-  mutate(future = map(m, make_future_dataframe, periods = 36, freq = "month"),
-         cap = case_when(
-           city == "sj" ~ 500,
-           city == "iq" ~ 200
-         ),
-         future = map2(future, cap, ~ {.x$cap <- .y; .x}),
-         forecast = map2(m, future, predict))
 
-train_nested <- train_nested %>% 
-  mutate(p = map2(m, forecast, plot),
-         pc = map2(m, forecast, prophet_plot_components))
-train_nested$p
+dengue_nested$forecast[[1]] %>% 
+  mutate(ds = lubridate::ymd(ds)) %>% 
+  select(ds, yhat, yhat_lower, yhat_upper) %>% 
+  left_join(select(dengue_nested$test[[1]], ds, y)) %>% 
+  mutate_at(2:5, ~ exp(.x) - 1)
 
+
+
+dengue_nested <- dengue_nested %>% 
+  mutate(p = map2(mod, forecast, plot),
+         pc = map2(mod, forecast, prophet_plot_components))
+dengue_nested$p
+dengue_nested %>% 
+  mutate(p = map2(p, test, ~ .x + geom_point(aes(ds, y), data = .y, color = "red")))
+p <- dengue_nested$p[[1]]
